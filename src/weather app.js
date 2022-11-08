d4a98773dc79b0482f839d1c18d240b60cccaa5d
pao050988 <pao050988@gmail.com>
2022-11-08 10:51:33 -0500

    edited html
    changed the html, edited the java script creating the forecast for the week, create a loop for the forecast

--- src/weather app.js
+++ src/weather app.js
@@ -21,6 +21,34 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+              <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img
+                  src="http://openweathermap.org/img/wn/04d@2x.png"
+                  alt=""
+                  width="42"
+                />
+                <div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-temperature-max">18°</span>
+                  <span class="weather-forecast-temperature-min">12°</span>
+                </div>
+              </div>
+            `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   celsiusTemperature = response.data.main.temp;
@@ -83,6 +111,8 @@ function displaycelsiusTemperature(event) {
 }
 
 let celsiusTemperature = null;
+displayForecast();
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
